0792e1286787773caefc223ba8fa3231d6a06740
Younes284 <141578638+Younes284@users.noreply.github.com>
2023-08-27 23:46:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,95 @@
 const types = [
       {
-        id:burger,
+        id:"burger",
         type: "burger",
       },
       {
-        id:pizza,
+        id:"pizza",
         type: "pizza",
       },
       {
-        id:snack,
+        id:"snack",
         type: "snack",
       },
       {
-        id:drink,
+        id:"drink",
         type: "drink",
       },
       {
-        id:dessert,
+        id:"dessert",
         type: "dessert",
       },
 ];
+
+const burgerlist=[
+    {
+        name:"beef burger",price:"3$"
+    },
+    {
+        name:"cheese burger",price:"4$"
+    },
+]
+const pizzalist=[
+    {
+        name:"veg burger",price:"5$"    
+    },
+    {
+        name:"peperoni burger",price:"7$"
+    },
+]
+const snacklist=[]
+const drinklist=[]
+const dessertlist=[]
+
 const typeDisplayDiv = document.getElementById('typeDisplay');
 
 const ulElement = document.createElement('ul');
+ ulElement.style.listStyleType = 'none';
+ ulElement.style.display = 'flex';
+ ulElement.style.flexDirection = 'row';
+ ulElement.style.justifyContent=' center';
 
 
 for (const item of types) {
-  const liElement = document.createElement('li');
-  liElement.textContent = item.type;
-  ulElement.appendChild(liElement);
+  const buttonElement = document.createElement('button');
+  buttonElement.textContent = item.type;
+  buttonElement.type = 'button';
+  buttonElement.style.margin='20px',
+  buttonElement.style.fontSize='40px',
+  buttonElement.style.padding='20px',
+  buttonElement.style.border = '1px solid ',
+  
+
+  buttonElement.onmouseover=function() {
+    buttonElement.style.backgroundColor = 'yellow';
+    buttonElement.style.color = 'white';
+  }
+  buttonElement.onmouseout = function() {
+    buttonElement.style.backgroundColor = 'white';
+    buttonElement.style.color = 'black';
+  };
+  ulElement.appendChild(buttonElement);
+
+  
 }
 typeDisplayDiv.appendChild(ulElement);
+burger.addEventListener('click',()=>{
+    const ulElement = document.createElement('ul');
+
+    for (const item of burgerlist) {
+        const liElement = document.createElement('li');
+        liElement.textContent = item.burgerlist;
+  }}
+  )
+  
+
+
+
+
+
+
+
+
   
   
   
